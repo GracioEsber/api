b8cc0daa12ffb53de9cff233ea9f4d2ce11b6cb9
Gracio Esber <gracioesber20@gmail.com>
2025-05-23 12:30:55 +0300

    C1

---
+++ gracio.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, Express!');
+});
+var data = [{msg:'Hi',id:1},{msg:'Hru',id:2},{msg:'Good',id:3}];
+app.get('/data', (req, res) => {
+  res.send(data);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
