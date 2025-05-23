9d15e7a910410d0f973aa4cb00f0f842ceb44111
Gracio Esber <gracioesber20@gmail.com>
2025-05-23 13:48:32 +0300

    C6

--- gracio.js
+++ gracio.js
@@ -1,16 +1,21 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
-const port = 3000;
-var cors = require('cors')
-app.use(cors())
+app.use(cors());
+
+const port = process.env.PORT || 3000;
+
+const data = [{msg:'Hi',id:1},{msg:'Hru',id:2},{msg:'Good',id:3}];
+
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
-var data = [{msg:'Hi',id:1},{msg:'Hru',id:2},{msg:'Good',id:3}];
+
 app.get('/data', (req, res) => {
-  res.send(data);
+  res.json(data); // use .json() instead of .send(data)
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server running on port ${port}`);
 });
